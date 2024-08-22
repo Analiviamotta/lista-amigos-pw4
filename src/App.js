a9f683a73bd4878f9be3956fadce947144cfb9aa
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-08-21 23:56:19 -0300

    feat: implement add friend functionality through form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [friend, setFriend] = useState({ name: "", idade: "", tel: "" });
+
+  function renderFriendsList() {
+    return (
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+           Nome: {item.name}, Idade: {item.idade}, tel {item.tel}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  function onChangeFriend(type, value) {
+    setFriend({ ...friend, [type]: value });
+  }
+
+  function addFriend(e) {
+    e.preventDefault();
+    setList([...list, friend]);
+    setFriend({ name: "", idade: "", tel: "" });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Lista de amigos</p>
+        <form onSubmit={addFriend}>
+          <label htmlFor="name">Nome: </label>
+          <input
+            type="text"
+            onChange={(e) => onChangeFriend("name", e.target.value)}
+            required
+          />
+
+          <label htmlFor="idade">Idade: </label>
+          <input
+            type="text"
+            onChange={(e) => onChangeFriend("idade", e.target.value)}
+            required
+          />
+
+          <label htmlFor="tel">Telefone: </label>
+          <input
+            type="tel"
+            onChange={(e) => onChangeFriend("tel", e.target.value)}
+            required
+          />
+          <button type="submit">Adicionar amigo</button>
+        
+        </form>
+        {renderFriendsList()}
       </header>
     </div>
   );
